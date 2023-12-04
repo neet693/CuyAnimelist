c85dc0b4707a6c2ea34c2c3c973970f359c74405
unknown <neet693@gmail.com>
2023-12-05 02:52:22 +0700

    Sesi 1 CuyAnimelist

---
+++ src/app/search/page.js
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <>
+      <h1>Search Page</h1>
+      <p>Ini adalah Search Page</p>
+    </>
+  );
+};
+export default Page;
+
+// export default function Page() {
+//   return <div>Search Pages</div>;
+// }
+
+// Const Arrow Fucntion
+//  const Page = () => <div>Search Page</div>
+//  export default Page
---
+++ src/components/Search/index.js
@@ -0,0 +1,8 @@
+const Search = () => {
+  return (
+    <div>
+      <h1 className="text-center">Search</h1>
+    </div>
+  );
+};
+export default Search;
